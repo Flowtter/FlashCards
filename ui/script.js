eda4242bd1ed3417e29a7b97f4be77362bf344ae
Naexys <naexys.naexys@gmail.com>
2020-11-18 16:22:30 +0100

    Frontend done

--- ui/script.js
+++ ui/script.js
@@ -1,6 +1,15 @@
-var cards = ""
+var questions = [];
+var answers = [];
+var actual = 0;
+var question_show_state = false;
+
+function refresh() {
+
+}
 
 function fetch_request() {
+    questions = [];
+    answers = [];
     var container = document.getElementById("topic-container");
     container.innerHTML = "";
     container = document.getElementById("sub-topic-container");
@@ -16,7 +25,6 @@ function fetch(data, container) {
     var i;
     for (i = 1; i < datas.length; i++) {
         datas[i] = datas[i].split(",")[0];
-        console.log(datas[i]);
         createBubble(datas[i], container);
     }
 }
@@ -43,7 +51,13 @@ function fetch_subtopic(name) {
 }
 
 function startGame(data) {
-    cards = data;
+    var cards = data.split('\n');
+    for (i = 0; i < cards.length; i++) {
+        split = cards[i].split(',')
+        questions.push(split[0])
+        answers.push(split[1])
+    }
+    new_question();
 }
 
 function fetch_subject(name) {
@@ -55,4 +69,49 @@ function fetch_subject(name) {
     container.style.display = "flex";
     axios.get("/subject/" + name)
     .then(function (reponse){startGame(reponse.data.payload);})
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+function new_question() {
+    console.log(questions)
+    console.log(answers)
+    if (questions.length != 0) {
+        question_show_state = false;
+        actual = getRandomInt(questions.length)
+        var question = document.getElementById('game-question');
+        question.innerHTML = questions[actual];
+    } else {
+        no_more_cards();
+    }
+}
+
+function display() {
+    if (questions.length != 0) {
+        var text = document.getElementById('game-question');
+        console.log(question_show_state)
+        if (question_show_state) {
+            text.innerHTML = questions[actual];
+            question_show_state = false;
+        } else {
+            text.innerHTML = answers[actual];
+            question_show_state = true;
+        }
+    }
+}
+
+function no_more_cards() {
+    var text = document.getElementById('game-question');
+    text.innerHTML = "no more cards";
+}
+
+function correct() {
+    answers[actual] = answers[answers.length-1];
+    questions[actual] = questions[questions.length-1];
+    answers.pop();
+    questions.pop();
+    new_question();
 }
